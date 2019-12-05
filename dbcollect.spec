16440721e0eff88de64aee3d6adf8b7225538175
Bart Sjerps <bart@outrun.nl>
2019-12-05 09:51:00 +0100

    Move to Extras, minor updates

--- dbcollect.spec
+++ dbcollect.spec
@@ -1,10 +1,11 @@
 Name:		dbcollect
 Summary:	Collect AWR/Statspack and system info
-Version:	1.0.4
+Version:	1.0.6
 Release:	1%{?dtap}
 License:	GPLv3+
 Group:		Applications/Databases
 Source0:	%{name}-%{version}.tbz2
+Distribution:	Outrun Extras
 BuildArch:	noarch
 
 %description
@@ -12,7 +13,7 @@ This tool collects AWR reports for all normal database instances listed in /etc/
 For this, sqlplus is called with @collect-awr to generate the AWRs.
 Also collects cpuinfo, meminfo, and partition info from /proc.
 The AWR files as well as the system info reports are placed in a ZIP file named
-/var/tmp/collectawr.zip
+/tmp/<hostname>-collectawr.zip
 
 %prep
 %setup -q -n %{name}
--- src/share/update_feature_usage.sql
+++ src/share/update_feature_usage.sql
@@ -1,5 +1,12 @@
 -----------------------------------------------------------------------------
--- Title       : update_feature_usager.sql
+-- Title       : update_feature_usage.sql
 -- Description : Updates feature usage statistics
 -----------------------------------------------------------------------------
+-- If AWR snapshots are enabled but an AWR report has never been generated,
+-- the feature_usage will show count 0 for feature 'AWR Report'.
+-- To update, run an AWR report once, for example 
+-- using @?/rdbms/admin/awrrpti
+-- Afterwards, run this script to update the usage tables.
+-- Only do this if you are licensed for Diagnostics and Tuning pack!
+
 EXEC DBMS_FEATURE_USAGE_INTERNAL.exec_db_usage_sampling(SYSDATE);
