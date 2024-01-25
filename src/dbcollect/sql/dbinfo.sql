5c1ff596f865dad2b9df49bf9903ecb1e70b038e
Bart Sjerps <bart@dirty-cache.com>
2024-01-25 14:10:02 +0100

    improve awr retention report

--- src/dbcollect/sql/dbinfo.sql
+++ src/dbcollect/sql/dbinfo.sql
@@ -554,18 +554,13 @@ PROMPT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 PROMPT AWR RETENTION
 PROMPT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-COL METRIC  FORMAT A20    HEAD 'Metric'
-COL MINUTES FORMAT 999999 HEAD 'Minutes'
+COL DBID       FORMAT 9999999999
+COL INTERVAL   FORMAT 999999 HEAD 'Interval'
+COL RETENTION  LIKE INTERVAL HEAD 'Retention'
 
-SELECT 'Interval' Metric
-, extract(day FROM snap_interval)*24*60 +
-  extract(hour FROM snap_interval)*60 +
-  extract(minute FROM snap_interval) MINUTES
-FROM dba_hist_wr_control UNION ALL
-SELECT 'Retention'
-, extract(day FROM retention)*24*60 +
-  extract(hour FROM retention)*60 +
-  extract(minute FROM retention) MINUTES
+SELECT dbid
+, extract(day FROM snap_interval)*24*60 + extract(hour FROM snap_interval)*60 + extract(minute FROM snap_interval) interval
+, extract(day FROM retention)*24*60     + extract(hour FROM retention)*60     + extract(minute FROM retention)     retention
 FROM dba_hist_wr_control
 /
 
