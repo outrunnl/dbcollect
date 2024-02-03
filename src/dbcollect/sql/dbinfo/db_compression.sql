c9c461483baff79fe91e68ec70beb70558d236b1
Bart Sjerps <bart@dirty-cache.com>
2024-02-03 14:26:57 +0100

    dbcollect compression_info improvements

--- src/dbcollect/sql/dbinfo/db_compression.sql
+++ src/dbcollect/sql/dbinfo/db_compression.sql
@@ -8,30 +8,29 @@ COL TABLES      FORMAT 999990            HEAD 'Tables'
 COL PARTITIONS  LIKE TABLES              HEAD 'Partitions'
 COL DATASIZE    FORMAT 99,999,999,990.99 HEAD 'Datasize'
 COL ALLOCATED   LIKE DATASIZE            HEAD 'Allocated'
-COL FREE        LIKE DATASIZE            HEAD 'Free'
+COL EMPTY       LIKE DATASIZE            HEAD 'Empty'
 COL RATIO       FORMAT 990.99            HEAD 'Ratio'
 
 BREAK ON REPORT
 COMPUTE SUM LABEL "Total" OF TABLES PARTITIONS DATASIZE ALLOCATED FREE ON REPORT
 
 SELECT '-' pdb_name
-, COALESCE(t.compress_for,'NONE')      compression
-, SUM(tbl)                             tables
-, SUM(part)                            partitions
-, SUM(bytes/1048576)                   datasize
-, SUM(block_size*blocks/1048576)       allocated
-, SUM(block_size*empty_blocks/1048576) free
-, SUM(bytes)/sum(block_size*blocks)    ratio
+, COALESCE(t.compress_for,'NONE') compression
+, SUM(tbl)                     tables
+, SUM(part)                    partitions
+, SUM(datasize/1048576)        datasize
+, SUM(allocated/1048576)       allocated
+, SUM(empty/1048576)           empty
+, SUM(datasize)/sum(allocated) ratio
 FROM (
-  SELECT tablespace_name, num_rows, 1 tbl, 0 part, compress_for,
-         blocks, empty_blocks, avg_row_len*num_rows bytes, avg_space
-  FROM dba_tables
+  SELECT tablespace_name, 1 tbl, 0 part, t.compress_for, blocks, block_size*empty_blocks empty, avg_row_len*num_rows datasize, avg_space, block_size*blocks allocated
+  FROM dba_tables t
+  JOIN dba_tablespaces USING (tablespace_name)
   UNION ALL
-  SELECT tablespace_name, num_rows, 0 tbl, 1 part, compress_for,
-        blocks, empty_blocks, avg_row_len*num_rows bytes, avg_space
-  FROM dba_tab_partitions
+  SELECT tablespace_name, 0 tbl, 1 part, p.compress_for, blocks, block_size*empty_blocks empty, avg_row_len*num_rows datasize, avg_space, block_size*blocks allocated
+  FROM dba_tab_partitions p
+  JOIN dba_tablespaces USING (tablespace_name)
 ) t
-JOIN dba_tablespaces USING (tablespace_name)
 WHERE blocks > 0
 GROUP BY t.compress_for
 ORDER BY t.compress_for NULLS FIRST
--- src/dbcollect/sql/dbinfo/pdb_compression.sql
+++ src/dbcollect/sql/dbinfo/pdb_compression.sql
@@ -8,31 +8,31 @@ COL TABLES      FORMAT 999990            HEAD 'Tables'
 COL PARTITIONS  LIKE TABLES              HEAD 'Partitions'
 COL DATASIZE    FORMAT 99,999,999,990.99 HEAD 'Datasize'
 COL ALLOCATED   LIKE DATASIZE            HEAD 'Allocated'
-COL FREE        LIKE DATASIZE            HEAD 'Free'
+COL EMPTY       LIKE DATASIZE            HEAD 'Empty'
 COL RATIO       FORMAT 990.99            HEAD 'Ratio'
 
 BREAK ON REPORT
 COMPUTE SUM LABEL "Total" OF TABLES PARTITIONS DATASIZE ALLOCATED FREE ON REPORT
 
 SELECT COALESCE(name, '-') pdb_name
-, COALESCE(t.compress_for,'NONE')         compression
-, SUM(tbl)                                tables
-, SUM(part)                               partitions
-, SUM(bytes/1048576)                      datasize
-, SUM(ct.block_size*blocks/1048576)       allocated
-, SUM(ct.block_size*empty_blocks/1048576) free
-, SUM(bytes)/sum(ct.block_size*blocks)    ratio
+, COALESCE(t.compress_for,'NONE') compression
+, SUM(tbl)                     tables
+, SUM(part)                    partitions
+, SUM(datasize/1048576)        datasize
+, SUM(allocated/1048576)       allocated
+, SUM(empty/1048576)           empty
+, SUM(datasize)/sum(allocated) ratio
 FROM (
-  SELECT con_id, tablespace_name, num_rows, 1 tbl, 0 part, compress_for,
-         blocks, empty_blocks, avg_row_len*num_rows bytes, avg_space
-  FROM cdb_tables
+  SELECT    name, 1 tbl, 0 part, t.compress_for, blocks, ct.block_size * empty_blocks empty, avg_row_len*num_rows datasize, ct.block_size*blocks allocated
+  FROM      cdb_tables t
+  JOIN      cdb_tablespaces ct USING (con_id, tablespace_name)
+  LEFT JOIN v$pdbs USING (con_id)
   UNION ALL
-  SELECT con_id, tablespace_name, num_rows, 0 tbl, 1 part, compress_for,
-         blocks, empty_blocks, avg_row_len*num_rows bytes, avg_space
-  FROM cdb_tab_partitions
+  SELECT    name, 0 tbl, 1 part, p.compress_for, blocks, ct.block_size * empty_blocks empty, avg_row_len*num_rows datasize, ct.block_size*blocks allocated
+  FROM      cdb_tab_partitions p
+  JOIN      cdb_tablespaces ct USING (con_id, tablespace_name)
+  LEFT JOIN v$pdbs USING (con_id)
 ) t
-JOIN cdb_tablespaces ct USING (con_id, tablespace_name)
-LEFT JOIN v$pdbs USING (con_id)
 WHERE blocks > 0
 GROUP BY name, t.compress_for
 ORDER BY name, t.compress_for NULLS FIRST
