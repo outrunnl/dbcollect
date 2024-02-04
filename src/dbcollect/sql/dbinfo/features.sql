740773b6c52afb944a22e90758c51ae6facc264d
Bart Sjerps <bart@dirty-cache.com>
2024-02-04 20:01:33 +0100

    update dbinfo features

--- src/dbcollect/sql/dbinfo/features.sql
+++ src/dbcollect/sql/dbinfo/features.sql
@@ -1,22 +1,31 @@
 PROMPT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 PROMPT DATABASE FEATURE USAGE
 PROMPT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- Thanks to Oracle-base, see https://oracle-base.com/articles/misc/tracking-database-feature-usage
 
-COL FEATURE FORMAT A55   HEAD 'Feature'
-COL USAGES  FORMAT 9,999 HEAD 'Usage'
-COL INUSE   FORMAT A10   HEAD 'In Use'
-COL VERSION FORMAT A14   HEAD 'Version'
+COL NAME                   FORMAT A55   HEAD 'Feature'
+COL DBID                   FORMAT 9999999999
+COL VERSION                FORMAT A14   HEAD 'Version'
+COL DETECTED_USAGES        FORMAT 9999  HEAD 'Count'
+COL CURRENTLY_USED         FORMAT A10   HEAD 'In Use'
+COL FIRST_USAGE_DATE                    HEAD 'First Usage'
+COL LAST_USAGE_DATE                     HEAD 'Last Usage'
+COL LAST_SAMPLE_DATE                    HEAD 'Last Sample'
+COL AGE                    FORMAT 999   HEAD 'Age'
+COL DESCRIPTION            FORMAT A160  HEAD 'Description'
 
-SELECT u1.name feature, u1.detected_usages usages, u1.currently_used inuse, u1.version version
--- , u1.first_usage_date, u1.last_usage_date, u1.description
-FROM   dba_feature_usage_statistics u1
-WHERE  u1.version = (SELECT MAX(u2.version)
-                     FROM   dba_feature_usage_statistics u2
-                     WHERE  u2.name = u1.name)
-AND    u1.detected_usages > 0
-AND    u1.dbid = (SELECT dbid FROM v$database)
-ORDER BY name
+SELECT name
+, dbid
+, version
+, detected_usages
+, currently_used
+, first_usage_date
+, last_usage_date
+, last_sample_date
+, round(last_sample_date - last_usage_date) age
+, description
+FROM  dba_feature_usage_statistics
+WHERE DETECTED_USAGES > 0
+ORDER BY dbid, name, version
 /
 
 CLEAR COMPUTES COLUMNS
