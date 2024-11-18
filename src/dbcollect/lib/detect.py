5518d945d08a34f2e32f8554f7628acfc3fedc4b
Bart Sjerps <bart@dirty-cache.com>
2024-11-18 13:47:37 +0100

    add remote feature

--- src/dbcollect/lib/detect.py
+++ src/dbcollect/lib/detect.py
@@ -207,14 +207,9 @@ def get_instances(args):
         hclist    = get_hcfiles(orahomes)
 
     runlist = running_instances()
-
-    # TBD - work in progress
-    """
-    if args.dbcreds and args.dbremote:
-        for sid in creds:
-            if not sid in runlist:
-                runlist[sid] = { 'connectstring': creds[sid] }
-    """
+    if args.remote:
+        for sid in creds.keys():
+            runlist[sid] = {}
 
     for sid in runlist:
         connectstring = None
